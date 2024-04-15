a5179a328c28e425af321172daae954c0a47b731
Lucas Mendonça <lucas_lam@live.com>
2024-04-15 00:01:49 -0300

    💌

---
+++ Assets/_Scripts/History/HistoryEntry.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HistoryEntry : MonoBehaviour
+{
+    public HistorySphere[] entries;
+
+    public void InitializeEntry(int[] Results)
+    {
+
+        for(int result = 0; result < Results.Length; result++)
+        {
+            entries[result].UpdateMaterial(Results[result]);
+        }
+    }
+}
---
+++ Assets/_Scripts/History/HistorySphere.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HistorySphere : MonoBehaviour
+{
+    public Material WrongMaterial;
+    public Material MisplacedMaterial;
+    public Material CorrectMaterial;
+    public void UpdateMaterial(int result)
+    {
+        Renderer r = GetComponent<Renderer>();
+        switch (result)
+        {
+            case -1:
+                r.SetMaterials(new List<Material>() { WrongMaterial });
+                break;
+            case 0:
+                r.SetMaterials(new List<Material>() { MisplacedMaterial });
+                break; 
+            case 1:
+                r.SetMaterials(new List<Material>() { WrongMaterial });
+                break;
+
+        }
+    }
+        
+    
+
+}
