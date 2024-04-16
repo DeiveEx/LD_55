6b52763c57cf6efbbf0628987d574de081fbd8e0
Deive_Ex <feliperobos@gmail.com>
2024-04-15 21:52:34 -0300

    Tutorial Slide

--- Assets/_Scripts/UI/UI_InstructionScreen.cs
+++ Assets/_Scripts/UI/UI_InstructionScreen.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UI_InstructionScreen : MonoBehaviour
 {
     [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private GameObject[] _slides;
+    
     public bool isOpen = false;
+    private int _currentSlideIndex;
 
     private void Awake()
     {
@@ -30,6 +30,8 @@ public class UI_InstructionScreen : MonoBehaviour
         canvasGroup.alpha = 1;
         canvasGroup.interactable = true;
         canvasGroup.blocksRaycasts = true;
+        
+        ShowSlide(0);
     }
 
     public void Close()
@@ -38,5 +40,34 @@ public class UI_InstructionScreen : MonoBehaviour
         canvasGroup.alpha = 0;
         canvasGroup.interactable = false;
         canvasGroup.blocksRaycasts = false;
+        
+        for (int i = 0; i < _slides.Length; i++)
+        {
+            _slides[i].SetActive(false);
+        }
+    }
+
+    public void ShowSlide(int slideIndex)
+    {
+        _currentSlideIndex = slideIndex;
+        
+        for (int i = 0; i < _slides.Length; i++)
+        {
+            _slides[i].SetActive(i == slideIndex);
+        }
+    }
+
+    public void ShowNextSlide()
+    {
+        _currentSlideIndex++;
+
+        if (_currentSlideIndex >= _slides.Length)
+        {
+            Close();
+        }
+        else
+        {
+            ShowSlide(_currentSlideIndex);
+        }
     }
 }
