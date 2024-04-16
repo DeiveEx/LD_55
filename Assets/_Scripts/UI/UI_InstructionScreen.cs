520a8ceec439c97ac9676501c6ef1c9ba6824136
Deive_Ex <feliperobos@gmail.com>
2024-04-15 21:10:19 -0300

    Hide Tutorial Screen On Awake

--- Assets/_Scripts/UI/UI_InstructionScreen.cs
+++ Assets/_Scripts/UI/UI_InstructionScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,11 @@ public class UI_InstructionScreen : MonoBehaviour
 {
     [SerializeField] private CanvasGroup canvasGroup;
     public bool isOpen = false;
+
+    private void Awake()
+    {
+        Close();
+    }
 
     private void Update()
     {
